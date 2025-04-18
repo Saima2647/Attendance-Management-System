437b7c636f3122619448616e9a867dd7e3de5ad4
Saima2647 <saimaarora744@gmail.com>
2025-04-18 14:34:11 +0530

    Update attendance_system (6).sql

--- att/db/attendance_system (6).sql
+++ att/db/attendance_system (6).sql
@@ -50,33 +50,6 @@ CREATE TABLE `attendance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`id`, `student_id`, `subject_id`, `attendance_date`, `status`) VALUES
-(2, 6, 37, '2025-04-12', 'present'),
-(3, 16, 37, '2025-04-12', 'present'),
-(4, 6, 39, '2025-04-12', 'absent'),
-(5, 16, 39, '2025-04-12', 'present'),
-(6, 6, 37, '2025-04-13', 'present'),
-(7, 16, 37, '2025-04-13', 'late'),
-(8, 6, 36, '2025-04-12', 'present'),
-(9, 16, 36, '2025-04-12', 'present'),
-(10, 7, 38, '2025-04-12', 'present'),
-(11, 6, 39, '2025-04-13', 'absent'),
-(12, 16, 39, '2025-04-13', 'present'),
-(13, 6, 37, '2025-04-15', 'absent'),
-(14, 16, 37, '2025-04-15', 'late'),
-(15, 6, 37, '2025-04-14', 'present'),
-(16, 16, 37, '2025-04-14', 'late'),
-(17, 6, 37, '2025-04-15', 'late'),
-(18, 16, 37, '2025-04-15', 'absent'),
-(19, 6, 39, '2025-04-14', 'present'),
-(20, 16, 39, '2025-04-14', 'absent');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `class_schedule`
 --
 
@@ -90,16 +63,6 @@ CREATE TABLE `class_schedule` (
   `end_time` time NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `class_schedule`
---
-
-INSERT INTO `class_schedule` (`id`, `class_title`, `subject_id`, `teacher_id`, `class_date`, `start_time`, `end_time`, `created_at`) VALUES
-(27, 'ds', 36, 2, '2025-04-18', '09:20:00', '11:00:00', '2025-04-14 15:35:20');
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `courses`
 --
@@ -108,19 +71,6 @@ CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
   `course_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `courses`
---
-
-INSERT INTO `courses` (`id`, `course_name`) VALUES
-(12, 'BBA'),
-(14, 'BCA'),
-(11, 'MBA'),
-(10, 'MCA');
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `registration_codes`
 --
@@ -131,19 +81,6 @@ CREATE TABLE `registration_codes` (
   `role` enum('student','teacher','admin') NOT NULL,
   `used` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `registration_codes`
---
-
-INSERT INTO `registration_codes` (`id`, `code`, `role`, `used`) VALUES
-(33, '6CF67538', 'teacher', 0),
-(34, '9D0C6933', 'teacher', 0),
-(35, '31543807', 'admin', 0),
-(36, 'BB25B42B', 'admin', 0);
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `student_subjects`
 --
@@ -153,25 +90,6 @@ CREATE TABLE `student_subjects` (
   `student_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `student_subjects`
---
-
-INSERT INTO `student_subjects` (`id`, `student_id`, `subject_id`) VALUES
-(37, 7, 35),
-(38, 6, 36),
-(39, 16, 36),
-(41, 6, 37),
-(42, 16, 37),
-(44, 7, 38),
-(45, 6, 39),
-(46, 16, 39),
-(48, 6, 40),
-(49, 16, 40);
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `subjects`
 --
@@ -182,23 +100,6 @@ CREATE TABLE `subjects` (
   `teacher_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`id`, `subject_name`, `teacher_id`, `course_id`) VALUES
-(27, 'Digital Marketing', 2, 11),
-(35, 'Operation Management ', 3, 11),
-(36, 'Data Structure', 2, 10),
-(37, 'Python', 2, 10),
-(38, 'Finance', 3, 11),
-(39, 'Digital Marketing', 2, 10),
-(40, 'Internet of Things ', 2, 10),
-(41, 'Digital Marketing', 2, 12);
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `teacher_subjects`
 --
@@ -209,22 +110,6 @@ CREATE TABLE `teacher_subjects` (
   `subject_id` int(11) DEFAULT NULL,
   `course_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `teacher_subjects`
---
-
-INSERT INTO `teacher_subjects` (`id`, `teacher_id`, `subject_id`, `course_id`) VALUES
-(11, 2, 27, 11),
-(19, 3, 35, 11),
-(20, 2, 36, 10),
-(21, 2, 37, 10),
-(22, 3, 38, 11),
-(23, 2, 39, 10),
-(24, 2, 40, 10);
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
@@ -237,19 +122,6 @@ CREATE TABLE `users` (
   `role` enum('admin','teacher','student') NOT NULL,
   `course_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `course_id`) VALUES
-(2, 'Tanisha', 't@gmail.com', '$2y$10$4tdSXkxlpV7EaA/6HXKiZelFh2x1nL8KEhDNJP43a86WAstC22KXK', 'teacher', NULL),
-(3, 'ABC', 'abc@gmail.com', '$2y$10$GCWONidBQJys6Qd3qMrCHe4MLhAHXHuoZvwuXUM.5RBtskv52DxWO', 'teacher', NULL),
-(5, 'Siddhi', 'siddhi@gmail.com', '$2y$10$Iok8oIhD8wz8N8D070i6qeoT3xPhIzaTZs25J23Ud5pmjDJMNfzb2', 'admin', NULL),
-(6, 'Saima Arora ', 'saima@gmail.com', '$2y$10$jRoOInTWd48VvbH85AjZd.bX0sSoikEyA/TCaZhsnLvzQQSEWJ3xu', 'student', 10),
-(7, 'Mahak Goyal', 'm@gmail.com', '$2y$10$WAvuK1/68GFYgIC7.iWaKuYHqL1jXy6BSjR/eFPdmtPFdlLDFranO', 'student', 11),
-(16, 'Roohi ', 'roohi@gmail.com', '$2y$10$oFCIqUCW3i04PqswU4Kgo.4luQUbDLO7Iz0rciPpOQC9u/zKgbRau', 'student', 10);
-
 --
 -- Indexes for dumped tables
 --
